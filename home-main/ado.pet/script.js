235adc26cef876574033fe83a39da4251e53bd13
Ingred Conceição <84211250+ingredconceicao@users.noreply.github.com>
2023-06-07 11:07:37 -0300

    Add files via upload

---
+++ home-main/ado.pet/script.js
@@ -0,0 +1,14 @@
+const ham =document.querySelector(".hamburger")
+const navBar = document.querySelector(".links")
+
+ham.addEventListener('click',()=>{
+    ham.classList.toggle('active')
+    navBar.classList.toggle('active')
+
+});
+
+
+
+
+
+
